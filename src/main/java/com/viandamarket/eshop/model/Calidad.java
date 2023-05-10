237f18fe68323cc9e688ca15ed5672affd096fc6
Carolina Tovar <Carolina.ta13@gmail.com>
2023-05-10 15:36:12 -0500

    Se creo controller y service de calidades

--- src/main/java/com/viandamarket/eshop/model/Calidad.java
+++ src/main/java/com/viandamarket/eshop/model/Calidad.java
@@ -17,58 +17,58 @@ public class Calidad {
 		
 		Calidad.total++;
 		this.id=Calidad.total;
-	}
+	}//Constructor
 
 	public Calidad() {		
 		Calidad.total++;
 		this.id=Calidad.total;
-	}
+	}//constructor default
 
 	public String getMarca() {
 		return marca;
-	}
+	}//getMarca
 
 	public void setMarca(String marca) {
 		this.marca = marca;
-	}
+	}//setMarca
 
 	public String getPais() {
 		return pais;
-	}
+	}//getPais
 
 	public void setPais(String pais) {
 		this.pais = pais;
-	}
+	}//setPais
 
 	public String getDescripcion_marca() {
 		return descripcion_marca;
-	}
+	}//getDescripcion_marca
 
 	public void setDescripcion_marca(String descripcion_marca) {
 		this.descripcion_marca = descripcion_marca;
-	}
+	}//setDescripcion_marca
 
 	public String getCalidad() {
 		return calidad;
-	}
+	}//getCalidad
 
 	public void setCalidad(String calidad) {
 		this.calidad = calidad;
-	}
+	}//setCalidad
 
 	public long getId() {
 		return id;
-	}
+	}//getId
 
 	public void setId(long id) {
 		this.id = id;
-	}
+	}//setId
 
 	@Override
 	public String toString() {
 		return "Calidades [marca=" + marca + ", pais=" + pais + ", descripcion_marca=" + descripcion_marca
 				+ ", calidad=" + calidad + "]";
-	}
+	}//toString
 
 	
-}
+}//class
