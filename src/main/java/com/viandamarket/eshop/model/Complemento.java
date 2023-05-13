6ec377a84d518e78bc7830466519a8716b4989f9
Moicr96 <moicr96@gmail.com>
2023-05-12 18:06:22 -0600

    se sube parte de la tarea

--- src/main/java/com/viandamarket/eshop/model/Complemento.java
+++ src/main/java/com/viandamarket/eshop/model/Complemento.java
@@ -1,87 +1,99 @@
 package com.viandamarket.eshop.model;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+
+@Entity
+@Table(name="complementos")
 public class Complemento {
-	private String nombre;
-	private boolean disponibilidad;
-	private int cantidad_disponible;
-	private String descripcion_complemento;
-	private double precio;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="idcomplementos",unique=true,nullable=false)
 	private long id;
-	private static long total=0;
-//	public Complemento(String nombre, double d, String string, boolean b,
-//			double precio) {
-//		super();
-//		this.nombre = nombre;
-//		this.disponibilidad = d;
-//		this.cantidad_disponible = string;
-//		this.descripcion_complemento = b;
-//		this.precio = precio;
-//		Complemento.total++;
-//		this.id=Complemento.total;
-//	}
+	@Column(nullable=false)
+	private String nombre;
+	@Column(nullable=false)
+	private boolean disponibilidad;
+	@Column(nullable=false)
+	private int cantidad_disponible;
+	@Column(nullable=false)
+	private String descripcion_complemento;
+	@Column(nullable=false)
+	private float precio;
+	
 	public Complemento() {
-		Complemento.total++;
-		this.id=Complemento.total;
 	}
-	
-	public Complemento(String nombre, boolean disponibilidad, int cantidad_disponible, 
-			String descripcion_complemento,	double precio) {
-	super();
-	this.nombre = nombre;
-	this.disponibilidad = disponibilidad;
-	this.cantidad_disponible = cantidad_disponible;
-	this.descripcion_complemento = descripcion_complemento;
-	this.precio = precio;
-	Complemento.total++;
-	this.id=Complemento.total;
-}
-	public boolean isDisponibilidad() {
-		return disponibilidad;
-	}
-	
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
+
+	public Complemento(long id, String nombre, boolean disponibilidad, int cantidad_disponible,
+			String descripcion_complemento, float precio) {
+		this.id = id;
 		this.nombre = nombre;
-	}
-	public int getCantidad_disponible() {
-		return cantidad_disponible;
-	}
-	public void setCantidad_disponible(int cantidad_disponible) {
+		this.disponibilidad = disponibilidad;
 		this.cantidad_disponible = cantidad_disponible;
-	}
-	public String getDescripcion_complemento() {
-		return descripcion_complemento;
-	}
-	public void setDescripcion_complemento(String descripcion_complemento) {
 		this.descripcion_complemento = descripcion_complemento;
-	}
-	public double getPrecio() {
-		return precio;
-	}
-	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
-	public static long getTotal() {
-		return total;
+
+	public String getNombre() {
+		return nombre;
 	}
-	public static void setTotal(long total) {
-		Complemento.total = total;
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
+
+	public boolean isDisponibilidad() {
+		return disponibilidad;
+	}
+
 	public void setDisponibilidad(boolean disponibilidad) {
 		this.disponibilidad = disponibilidad;
-	}//setDisponibilidad
+	}
+
+	public int getCantidad_disponible() {
+		return cantidad_disponible;
+	}
+
+	public void setCantidad_disponible(int cantidad_disponible) {
+		this.cantidad_disponible = cantidad_disponible;
+	}
+
+	public String getDescripcion_complemento() {
+		return descripcion_complemento;
+	}
+
+	public void setDescripcion_complemento(String descripcion_complemento) {
+		this.descripcion_complemento = descripcion_complemento;
+	}
+
+	public float getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+
 	@Override
 	public String toString() {
-		return "Complementos [nombre=" + nombre + ", disponibilidad=" + disponibilidad + ", cantidad_disponible="
-				+ cantidad_disponible + ", descripcion_complemento=" + descripcion_complemento + ", precio=" + precio
-				+ ", id=" + id + "]";
-	}//toString	
+		return "Complemento [id=" + id + ", nombre=" + nombre + ", disponibilidad=" + disponibilidad
+				+ ", cantidad_disponible=" + cantidad_disponible + ", descripcion_complemento="
+				+ descripcion_complemento + ", precio=" + precio + "]";
+	}
+	
+	
+	
+	
 }
