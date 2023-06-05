35c6cb818b32a986411bd7abc8950517e661b57d
Angel Ordoñez <65252722+johnangelmx@users.noreply.github.com>
2023-06-05 12:46:53 -0600

    Delete src/main/resources/static directory

--- src/main/resources/static/assets/js/panel.js
+++
@@ -1,8 +0,0 @@
-const btnEditarDireccion = document.getElementById("btnAgregar");
-const formularioDireccion = document.querySelector(".fondoform");
-
-function toggleForm() {
-  formularioDireccion.classList.toggle("hide");
-}
-
-btnEditarDireccion.addEventListener("click", toggleForm);
