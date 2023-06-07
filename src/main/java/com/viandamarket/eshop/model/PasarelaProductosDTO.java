16752cd794058318c7544751a494b631af745bdc
frangersal <frangersal@outlook.com>
2023-06-07 00:13:03 -0600

    se agregaron modelos: PasarelaProductosDTO y ProductoDTO. y controlador: PasarelaPagosController

---
+++ src/main/java/com/viandamarket/eshop/model/PasarelaProductosDTO.java
@@ -0,0 +1,15 @@
+package com.viandamarket.eshop.model;
+
+import java.util.List;
+
+public class PasarelaProductosDTO {
+	 private List<ProductoDTO> productos;
+
+	    public List<ProductoDTO> getProductos() {
+	        return productos;
+	    }
+
+	    public void setProductos(List<ProductoDTO> productos) {
+	        this.productos = productos;
+	    }
+}
